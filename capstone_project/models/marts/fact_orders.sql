3ceb0841b907d9c4fb7336a1a3b273c002da8b31
Nguyen Tran Quoc Dat <10422017@student.vgu.edu.vn>
2025-10-26 17:44:33 +0700

    fix: change incremental strategy from 'append' to 'merge' in fact_orders model

--- capstone_project/models/marts/fact_orders.sql
+++ capstone_project/models/marts/fact_orders.sql
@@ -2,7 +2,7 @@
     materialized = 'incremental',
     unique_key='order_key',
     schema='sc_analytics',
-    incremental_strategy='append'
+    incremental_strategy='merge'
 ) }}
 
 with orders as (
