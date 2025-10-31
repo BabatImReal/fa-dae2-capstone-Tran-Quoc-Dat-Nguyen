c1ac72b710dff5bbfaca0a7d768c8202e5ec47f7
Nguyen Tran Quoc Dat <10422017@student.vgu.edu.vn>
2025-10-31 17:44:01 +0700

    fix: redesign the dim_product to make sure for fact table

--- capstone_project/models/marts/dim_products.sql
+++ capstone_project/models/marts/dim_products.sql
@@ -4,24 +4,50 @@
   cluster_by=['product_category_name_english']
 ) }}
 
-with src as (
-    select *
+with stg_products as (
+    select
+        product_id,
+        product_name_length,
+        product_description_length,
+        product_photos_qty,
+        product_weight_g,
+        product_length_cm,
+        product_height_cm,
+        product_width_cm,
+        loaded_at
+    from {{ ref('stg__products') }}
+),
+
+int_products as (
+    select
+        product_id,
+        product_category_name_english
     from {{ ref('int__products') }}
 )
 
 select
     -- surrogate key
-    {{ dbt_utils.generate_surrogate_key(['product_id', 'loaded_at']) }} as product_key,
-    -- natural/business keys and attributes
-    product_id,
-    product_category_name_english,
-    product_name_length,
-    product_description_length,
-    product_photos_qty,
-    product_weight_g,
-    product_length_cm,
-    product_height_cm,
-    product_width_cm,
+    {{ dbt_utils.generate_surrogate_key(['sp.product_id', 'sp.loaded_at']) }} as product_key,
+    
+    -- natural/business keys
+    sp.product_id,
+    
+    -- category from int__products (enriched with English translation)
+    ip.product_category_name_english,
+    
+    -- product attributes from stg__products
+    sp.product_name_length,
+    sp.product_description_length,
+    sp.product_photos_qty,
+    sp.product_weight_g,
+    sp.product_length_cm,
+    sp.product_height_cm,
+    sp.product_width_cm,
+    
     -- metadata
-    loaded_at
-from src
+    sp.loaded_at,
+    current_timestamp() as dbt_updated_at
+
+from stg_products as sp
+left join int_products as ip
+    on sp.product_id = ip.product_id
