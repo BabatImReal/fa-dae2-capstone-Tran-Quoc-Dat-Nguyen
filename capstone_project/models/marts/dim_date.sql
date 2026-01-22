8d172ff85b716eade805fc0a75c089b9b4242987
Nguyen Tran Quoc Dat <10422017@student.vgu.edu.vn>
2026-01-22 10:48:11 +0700

    feat: Simplify comment in dim_date.sql to enhance clarity of business logic

--- capstone_project/models/marts/dim_date.sql
+++ capstone_project/models/marts/dim_date.sql
@@ -24,7 +24,7 @@ date_attributes as (
             when month(date_value) in (7, 8, 9) then 'Q3'
             else 'Q4'
         end as fiscal_quarter,
-        -- Business logic for date
+        -- Business logic
         coalesce(dayofweekiso(date_value) in (6, 7), false) as is_weekend,
         coalesce(date_value = current_date(), false) as is_today
     from date_spine
