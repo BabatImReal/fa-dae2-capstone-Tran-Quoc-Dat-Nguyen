7132f2fb34deaeaddae9457e9a88657e9b0d38d9
Nguyen Tran Quoc Dat <10422017@student.vgu.edu.vn>
2026-01-23 21:33:12 +0700

    refactor: Correct comment for SCD Type 2 attributes in dim_customers.sql for consistency

--- capstone_project/models/marts/dim_customers.sql
+++ capstone_project/models/marts/dim_customers.sql
@@ -26,7 +26,7 @@ select
     snap.customer_city,
     snap.customer_state,
 
-    -- SCD Type 2 attribute
+    -- SCD Type 2 attributes
     snap.is_current,
     snap.effective_from,
     snap.effective_to,
